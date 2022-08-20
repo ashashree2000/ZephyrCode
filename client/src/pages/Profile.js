4f09c8445f3bb15f4da839d6b56d175cec4b0333
killerrings <swarjgogoi68@gmail.com>
2022-08-20 21:05:18 +0530

    Added entrire profile updation functionality

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,15 +1,41 @@
-import { Tabs, Row, Col, Input, Form } from "antd";
-import React from "react";
+import { Tabs, Row, Col, Input, Form, Button } from "antd";
+import React, { useState } from "react";
 import DefaultLayout from "../components/DefaultLayout";
+import { useDispatch } from "react-redux";
+import { updateUser } from "../redux/actions/userActions";
 const { TabPane } = Tabs;
 const { TextArea } = Input;
-const Profile = () => {
+
+function Profile() {
+  const [personalInfo, setPersonalInfo] = useState();
+  const [activeTab, setActiveTab] = useState("1");
+  const dispatch = useDispatch();
+
+  function onPersonInfoSubmit(values) {
+    setPersonalInfo(values);
+    console.log(values);
+    setActiveTab("2");
+  }
+
+  function onFinalFinish(values) {
+    const finalObj = { ...personalInfo, ...values };
+
+    console.log(finalObj);
+
+    dispatch(updateUser(finalObj));
+  }
+
+  const user = JSON.parse(localStorage.getItem("user"));
   return (
     <div>
       <DefaultLayout>
-        <Tabs>
+        <Tabs defaultActiveKey="1" activeKey={activeTab}>
           <TabPane tab="Personal Information" key="1">
-            <Form layout="vertical">
+            <Form
+              layout="vertical"
+              onFinish={onPersonInfoSubmit}
+              initialValues={user}
+            >
               <Row gutter={16}>
                 <Col lg={8} sm={24}>
                   <Form.Item
@@ -88,15 +114,198 @@ const Profile = () => {
                   </Form.Item>
                 </Col>
               </Row>
+              <Button type="primary" htmlType="submit">
+                Next
+              </Button>
             </Form>
           </TabPane>
           <TabPane tab="Skills and Education" key="2">
-            Content of Tab pane 2
+            <Form
+              initialValues={user}
+              layout="vertical"
+              onFinish={onFinalFinish}
+            >
+              <Row>
+                <Col lg={24} sm={24}>
+                  <Form.List name="education">
+                    {(education, { add, remove }) => (
+                      <div>
+                        {education.map((field, index) => (
+                          <div className="flex">
+                            <Form.Item
+                              required
+                              {...field}
+                              label="Education"
+                              style={{ width: "80%" }}
+                              rules={[{ required: true }]}
+                            >
+                              <TextArea rows={4} />
+                            </Form.Item>
+                            <Button
+                              style={{ marginLeft: "20px" }}
+                              onClick={() => {
+                                add();
+                              }}
+                            >
+                              Add more
+                            </Button>
+                            {index !== 0 && (
+                              <Button
+                                danger
+                                style={{ marginLeft: "10px" }}
+                                onClick={() => {
+                                  remove(index);
+                                }}
+                              >
+                                Delete
+                              </Button>
+                            )}
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                  </Form.List>
+                </Col>
+
+                <Col lg={24} sm={24}>
+                  <Form.List name="skills">
+                    {(skills, { add, remove }) => (
+                      <div>
+                        {skills.map((field, index) => (
+                          <div className="flex">
+                            <Form.Item
+                              required
+                              {...field}
+                              label="Skill"
+                              style={{ width: "80%" }}
+                              rules={[{ required: true }]}
+                            >
+                              <TextArea rows={4} />
+                            </Form.Item>
+                            <Button
+                              style={{ marginLeft: "20px" }}
+                              onClick={() => {
+                                add();
+                              }}
+                            >
+                              Add more
+                            </Button>
+                            {index !== 0 && (
+                              <Button
+                                danger
+                                style={{ marginLeft: "10px" }}
+                                onClick={() => {
+                                  remove(index);
+                                }}
+                              >
+                                Delete
+                              </Button>
+                            )}
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                  </Form.List>
+                </Col>
+
+                <Col lg={24} sm={24}>
+                  <Form.List name="projects">
+                    {(projects, { add, remove }) => (
+                      <div>
+                        {projects.map((field, index) => (
+                          <div className="flex">
+                            <Form.Item
+                              required
+                              {...field}
+                              label="Project"
+                              style={{ width: "80%" }}
+                              rules={[{ required: true }]}
+                            >
+                              <TextArea rows={4} />
+                            </Form.Item>
+                            <Button
+                              style={{ marginLeft: "20px" }}
+                              onClick={() => {
+                                add();
+                              }}
+                            >
+                              Add more
+                            </Button>
+                            {index !== 0 && (
+                              <Button
+                                danger
+                                style={{ marginLeft: "10px" }}
+                                onClick={() => {
+                                  remove(index);
+                                }}
+                              >
+                                Delete
+                              </Button>
+                            )}
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                  </Form.List>
+                </Col>
+                <Col lg={24} sm={24}>
+                  <Form.List name="experience">
+                    {(experience, { add, remove }) => (
+                      <div>
+                        {experience.map((field, index) => (
+                          <div className="flex">
+                            <Form.Item
+                              required
+                              {...field}
+                              label="Experience"
+                              style={{ width: "80%" }}
+                              rules={[{ required: true }]}
+                            >
+                              <TextArea rows={4} />
+                            </Form.Item>
+                            <Button
+                              style={{ marginLeft: "20px" }}
+                              onClick={() => {
+                                add();
+                              }}
+                            >
+                              Add more
+                            </Button>
+                            {index !== 0 && (
+                              <Button
+                                danger
+                                style={{ marginLeft: "10px" }}
+                                onClick={() => {
+                                  remove(index);
+                                }}
+                              >
+                                Delete
+                              </Button>
+                            )}
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                  </Form.List>
+                </Col>
+              </Row>
+              <Button
+                style={{ marginRight: "10px" }}
+                onClick={() => {
+                  setActiveTab("1");
+                }}
+              >
+                Previous
+              </Button>
+              <Button type="primary" htmlType="submit">
+                Update
+              </Button>
+            </Form>
           </TabPane>
         </Tabs>
       </DefaultLayout>
     </div>
   );
-};
+}
 
 export default Profile;
