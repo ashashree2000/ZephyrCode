371998854b2c2046cac041f9739486e1959272fe
killerrings <swarjgogoi68@gmail.com>
2022-08-20 13:24:51 +0530

    Added some frontend pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "antd/dist/antd.min.css";
+import Home from "./pages/Home";
+import JobInfo from "./pages/JobInfo";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/jobinfo" element={<JobInfo />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DefaultLayout from "../components/DefaultLayout";
+
+const Home = () => {
+  return (
+    <div>
+      <DefaultLayout>Homepage</DefaultLayout>
+    </div>
+  );
+};
+
+export default Home;
